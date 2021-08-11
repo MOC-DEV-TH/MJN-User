24c56f769a153449020211bf9eacd4de0bbf7224
Sai Hein Min Latt <heinminlatt.dev1@gmail.com>
2021-08-11 21:21:19 +0630

    set up screen

---
+++ lib/screens/accountScreen/AccountScreen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AccountScreen extends StatelessWidget {
+  static const routeName = '/account_screen';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Text('Account Screen'),);
+  }
+}
---
+++ lib/screens/paymentScreen/PaymentScreen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class PaymentScreen extends StatelessWidget {
+  static const routeName = '/payment_screen';
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Text('Payment'),);
+  }
+}
